23be851a40018f7b2aeaea86bb04689464bcf03b
girdot <girdot@gmail.com>
2020-10-20 16:58:17 -0700

    pulling in module requirements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 cd /girbot
+rm -rf commands
 git pull
+git reset --hard
 python3 -m pip install -r requirements.txt
+cd commands
+for i in $(echo ${MODULE_REPOS// /} | sed "s/,/ /g")
+do
+    git clone https://github.com/$i
+done
+find . -name "requirements.txt" -exec python3 -m pip install -r requirements.txt
+cd /girbot
 python3 main.py
