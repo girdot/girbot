f58162382878fb637f39864b7783b167dadf78da
Danial Cauley <girdot@gmail.com>
2023-02-10 12:41:18 -0800

    fixing old version hopefully

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ python3 -m pip install -r requirements.txt
 cd commands
 for i in $(echo ${MODULE_REPOS// /} | sed "s/,/ /g")
 do
-    git clone https://github.com/$i
+    git clone --branch girbot-old https://github.com/$i
 done
 find . -name "requirements.txt" -exec python3 -m pip install -r {} \;
 cd ..
